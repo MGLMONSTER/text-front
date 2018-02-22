f2d73c7722c73df3b889997df3fb81c23733a1f1
cjh <1418503647@qq.com>
2018-02-22 20:52:37 +0800

    繁体字工具添加导入导出功能

--- src/views/Chinese.vue
+++ src/views/Chinese.vue
@@ -10,17 +10,26 @@
                 <ui-radio class="radio" label="台湾" name="group" nativeValue="zh_TW" v-model="language"/>
             </div>
             <div class="btns">
-                <ui-raised-button class="btn" label="转成简体中文" primary @click="simplified"/>
-                <ui-raised-button class="btn" label="转成繁体中文" @click="traditional"/>
+                <ui-raised-button class="btn" label="转成简体" primary @click="simplified" :disabled="text.length === 0"/>
+                <ui-raised-button class="btn" label="转成繁体" @click="traditional" :disabled="text.length === 0"/>
                 <ui-raised-button class="btn" label="重新输入" @click="clear"/>
             </div>
-
             <ui-text-field v-model="result" hintText="" multiLine :rows="4" :rowsMax="10" v-if="result"/>
+            <div>
+                <a class="advanced" href="#" @click.prevent="toggleAdvanced">高级</a>
+            </div>
+            <div class="btns" v-if="advancedVisible">
+                <ui-raised-button class="btn file-btn" secondary label="从文件中导入">
+                    <input type="file" class="ui-file-button" @change="fileChange($event, 1)">
+                </ui-raised-button>
+                <ui-raised-button class="btn" label="导出文件" :disabled="result.length === 0" @click="exportFile"/>
+            </div>
         </section>
     </my-page>
 </template>
 
 <script>
+    const saveAs = window.saveAs
     const transverter = window.transverter
 
     export default {
@@ -37,7 +46,9 @@
                             to: '/chinese/help'
                         }
                     ]
-                }
+                },
+                advancedVisible: false,
+                filename: '云设繁体字工具导出文件.txt'
             }
         },
         mounted() {
@@ -60,6 +71,24 @@
             clear: function () {
                 this.text = ''
                 this.result = ''
+            },
+            toggleAdvanced() {
+                this.advancedVisible = !this.advancedVisible
+            },
+            fileChange(e, left) {
+                let file = e.target.files[0]
+                this.filename = file.name
+                this.result = ''
+                console.log(file.name)
+                let reader = new FileReader()
+                reader.onload = e => {
+                    this.text = e.target.result
+                }
+                reader.readAsText(file, 'utf-8')
+            },
+            exportFile() {
+                let blob = new Blob([this.result], {type: 'text/plain;charset=utf-8'})
+                saveAs(blob, this.filename)
             }
         }
     }
@@ -82,5 +111,10 @@
         .btn {
             margin-right: 8px;
         }
+    }
+    .advanced {
+        display: inline-block;
+        margin-bottom: 16px;
+        color: #999;
     }
 </style>
